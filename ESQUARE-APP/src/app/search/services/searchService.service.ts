a8f87ef8eb9d5cf8fccc8a88a7ebdfa64a85c22d
Gad Dahan <gadahan10@gmail.com>
2020-03-04 09:19:43 +0200

    logout

--- ESQUARE-APP/src/app/search/services/searchService.service.ts
+++ ESQUARE-APP/src/app/search/services/searchService.service.ts
@@ -6,16 +6,15 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
-export class SearchService {
-
-    googleApiUrl: string = "https://www.googleapis.com/books/v1/volumes?q=english+inauthor:ju&maxResults=5&key=YOURAPIKEY";
+export class SearchService {  
 
     constructor(private httpService: HttpClient) {}
 
     getBooks(searchTerm: string){
        
-        var url = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm + "&maxResults=20&key=YOURAPIKEY";
+        var url = `https://www.googleapis.com/books/v1/volumes?q=" ${searchTerm} "&maxResults=20&key=AIzaSyBcaVnYSRR3JzHvGm4ad1IMY5oGWz40ePI`;
         return this.httpService.get(url);
+ 
     }
 
     
